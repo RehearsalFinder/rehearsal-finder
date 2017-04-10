7d7decc38c02f338f8963c8708cacb1b4051c04f
John Michael McCasland <jmmccasland@gmail.com>
2017-04-10 18:16:14 -0500

    Map markers present

--- app/rehearsalspaces/controller.js
+++ app/rehearsalspaces/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   lat: 36.152660,
   lng: -86.776029,
-  zoom: 12,
+  zoom: 13,
   diamondSound: [36.195387, -86.788094],
   inkBuilding: [36.152642, -86.776051],
 });
