154acdb4f6064ded1bbf7da15df5fcd1cebb9a8d
John Michael McCasland <jmmccasland@gmail.com>
2017-04-11 10:22:46 -0500

    Generate list of mock spaces

--- app/rehearsalspaces/route.js
+++ app/rehearsalspaces/route.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        id: 1,
+        name: 'That 70s House',
+        area: 'Belmont',
+        city: 'Nashville',
+        state: 'TN',
+        costPerHour: '15'
+      },
+      {
+        id: 2,
+        name: 'Awesome Band Spot',
+        area: 'Hip Area',
+        city: 'Nashville',
+        state: 'TN',
+        costPerHour: '100'
+      },
+      {
+        id: 3,
+        name: 'Diamond Sound',
+        area: 'Metro Center',
+        city: 'Nashville',
+        state: 'TN',
+        costPerHour: '30'
+      }
+    ];
+  }
 });
