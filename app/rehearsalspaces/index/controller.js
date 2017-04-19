18b33dd78c0a628dafb864db66b84dbfca785c5e
John Michael McCasland <jmmccasland@gmail.com>
2017-04-19 11:56:28 -0500

    Update model to include coordinates

--- app/rehearsalspaces/index/controller.js
+++ app/rehearsalspaces/index/controller.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   lat: 36.152660,
   lng: -86.776029,
   zoom: 13,
+
   diamondSound: [36.195387, -86.788094],
-  inkBuilding: [36.152642, -86.776051],
+  inkBuilding: [36.152642, -86.776051]
 });
