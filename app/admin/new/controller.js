b1eacd0d3ba3afde87de77e6755ddb600993ea78
John Michael McCasland <jmmccasland@gmail.com>
2017-04-20 16:01:51 -0500

    Ryan's changes

--- app/admin/new/controller.js
+++ app/admin/new/controller.js
@@ -15,24 +15,26 @@ export default Ember.Controller.extend({
       const fetch = this.get('filesystem.fetch');
       const token = this.get('session.session.content.authenticated.token');
 
-      fetch('http//localhost:8080/spaces/upload', {
+      fetch('http://localhost:8080/spaces/upload', {
         method: 'POST',
         headers: {
-          accept: 'application.json',
+          accept: 'application/json',
           authorization: `Bearer ${token}`
         },
         body: {
-          file: this.model.spaceImg[0],
+          photo: this.model.spaceImg[0],
           name: this.model.name,
           city: this.model.city,
           state: this.model.state,
-          costPerHour: this.model.costPerHour,
-          squareFeet: this.model.squareFeet,
+          'street-address': this.model.streetAddress,
+          zip: this.model.zip,
+          'cost-per-hour': this.model.costPerHour,
+          'square-feet': this.model.squareFeet,
           amenities: this.model.amenities,
-          availableEquipment: this.model.availableEquipment,
-          spaceHostName: this.model.spaceHostName,
-          hostEmail: this.model.hostEmail,
-          hostPhone: this.model.hostPhone,
+          'available-equipment': this.model.availableEquipment,
+          'space-host-name': this.model.spaceHostName,
+          'host-email': this.model.hostEmail,
+          'host-phone': this.model.hostPhone,
           featured: this.model.featured,
           description: this.model.description,
           rules: this.model.rules
